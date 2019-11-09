2292197b62c323d80702715ff9a9c971933539ab
Jack Works <zjwpeter@gmail.com>
2019-11-09 19:35:45 +0800

    feat: enforce React props on Angular components on type level

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core'
-import './Counter'
+import { Counter } from './Counter'
 
 @Component({
   selector: 'app-counter',
   templateUrl: './counter.component.html'
 })
-export class CounterComponent implements OnInit {
+export class CounterComponent implements OnInit, ReactComponentProps<typeof Counter> {
   count = 0
-  onChange(newCount: CustomEvent<number>) {
-    this.count = newCount.detail
+  onChange($event: EventsOfComponent<typeof Counter>['onChange']) {
+    this.count = $event.detail
   }
 
   ngOnInit() {}
